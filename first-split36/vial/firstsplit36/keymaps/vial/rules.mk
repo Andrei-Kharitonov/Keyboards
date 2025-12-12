bfc63f91f3469b43e427192dda854bb2d7c20a0c
Andrei-Kharitonov <89729599+Andrei-Kharitonov@users.noreply.github.com>
2025-12-12 16:13:24 +0400

    firstsplit36 added vial, sofle updated layout

---
+++ first-split36/vial/firstsplit36/keymaps/vial/rules.mk
@@ -0,0 +1,21 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = yes
+OLED_ENABLE         = no
+OLED_DRIVER         = no
+EXTRAKEY_ENABLE     = no
+COMBO_ENABLE        = no
+NKRO_ENABLE         = yes
+SPLIT_KEYBOARD      = yes
+
+QMK_SETTINGS        = yes
+
+CAPS_WORD_ENABLE = no
+LAYER_LOCK_ENABLE = no
+REPEAT_KEY_ENABLE = no
+
+VIAL_INSECURE = yes
